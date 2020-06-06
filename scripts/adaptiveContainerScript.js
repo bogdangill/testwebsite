0ccdfdc63172590046dd9048e74b273b32fee196
bogdangill <herecomesthefog@gmail.com>
2020-06-06 15:08:57 +0500

    history-section flex-order fix & header/footer menu width fix

--- scripts/adaptiveContainerScript.js
+++ scripts/adaptiveContainerScript.js
@@ -1,11 +1,14 @@
 let firstContainer = document.querySelector('.variableContainer');
 let containerWide = document.querySelector('.containerWide');
 let containerNarrow = document.querySelector('.containerNarrow');
+let historySections = document.querySelectorAll('.history-section');
 
 let bottles = document.querySelector('.bottles');
 let newThings = document.querySelectorAll('.new-thing');
 
-// console.log('высота содержимого вместе с полями padding, но без полосы прокрутки: '+bottles.clientHeight);
+// for (let historySection of historySections) {
+//     console.log('высота содержимого вместе с полями padding, но без полосы прокрутки: '+historySection.clientHeight);
+// }
 // console.log('ыыы: '+bottles.clientWidth);
 
 // пытаюсь убрать лишние border-left у элементов new-thing
@@ -20,11 +23,11 @@ if (bottles.clientWidth > 484){
 
 //меняю контейнеры по разной ширине экрана
 //знаю, что проще создать отдельный класс-контейнер со своими особыми паддингами под медиазапросы, да и если даже javascipt отвалится, то по приницпу graceful degradation.. потом сделаю, а сейчас - вот:
-if (window.matchMedia("(max-width: 767.98px)").matches) {
+if (window.matchMedia("(max-width: 991.98px)").matches) {
   // firstContainer.classList.remove('containerNarrow');
   firstContainer.classList.toggle('containerWide');
   console.log('включен широкий контейнер');
-} else if (window.matchMedia('(min-width: 767.98px)').matches) {
+} else if (window.matchMedia('(min-width: 991.98px)').matches) {
   firstContainer.classList.toggle('containerNarrow');
   console.log('включен узкий контейнер');
 }
