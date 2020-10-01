0e3b3c2adee53c2e9044915d7c17fd8a1c3f81f6
bogdangill <herecomesthefog@gmail.com>
2020-10-01 20:01:41 +0500

    popups script refactoring

--- scripts/popups.js
+++ scripts/popups.js
@@ -1,25 +1,32 @@
 /*скрипт для всплывающих окон на сайте, которые сообщают о нерабочем состоянии некоторых кнопок*/
 
 let btns = document.querySelectorAll('.btn');
-let popUps = document.querySelectorAll('.pop-up');
+let submitButton = document.querySelector('.submitbutton');
 
-    for (let btn of btns) {
+function createElement(parentName, elementClassName, elementContent) {
 
-        btn.onclick = function () {
+    let element = document.createElement('div');
+    parentName.append(element);
+    element.classList.add(elementClassName);
+    element.textContent = elementContent;
 
-            let hintElement = document.createElement('div');//создаю пустой тег и объявляю его как переменную
-            btn.append(hintElement);
-            hintElement.classList.add('popup');
-            hintElement.textContent = 'Кнопка не работает. Это просто статичный демонстрационный лендинг-пустышка без PHP, API и прочих свистоперделок. Закройте окошко, нажав на крестик, который появится при наведении мыши в правом верхнем углу. Спасибо за внимание.';
-            let closeButton = document.createElement('div');
-            hintElement.append(closeButton);
-            closeButton.classList.add('popup-close');
-            closeButton.textContent = 'x';
+    return element;
+}
 
-            closeButton.onclick = function () {
-                btn.disabled = true;
-                hintElement.classList.remove('popup');
-                hintElement.textContent = '';
-            }
-        };
-    };
+for (let btn of btns) {
+
+    btn.addEventListener('click', function() {
+
+        let popup = createElement(btn, 'popup', 'Кнопка не работает. Это просто статичный демонстрационный лендинг-пустышка без PHP, API и прочих свистоперделок. Закройте окошко, нажав на крестик, который появится при наведении мыши в правом верхнем углу. Спасибо за внимание.');
+
+        let closeButton = createElement(popup, 'popup-close', 'x');
+
+        closeButton.onclick = function() { popup.remove(); btn.disabled = true; }
+
+    });
+};
+
+function charCounter(charField) {
+    document.getElementById('0').textContent = charField.value.length;
+    charField.value.length > 142 ? submitButton.disabled = true : submitButton.disabled = false;
+}
